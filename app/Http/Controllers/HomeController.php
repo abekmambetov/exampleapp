9edbad3495fe303902e0590265df6a962e111df5
Arthur Bekmambetov <arthurbekmambetov@gmail.com>
2022-11-17 00:56:16 +0300

    soe changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,13 +9,14 @@ use Illuminate\Support\Facades\Http;
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
 use Carbon\Carbon;
+use App\Services\PaymentService; 
 
 class HomeController extends Controller
 {    
-    private $limitPerDay = 20;
-    private $limitPerDayAdditional = 30;
-    private $hashFirstPayMethod = 'KaTf5tZYHx4v7pgZ';
-    private $hashSecondPayMethod = 'rTaasVHeteGbhwBx';
+    public function __construct(PaymentService $paymentService)
+    {
+        $this->paymentService = $paymentService;
+    }
 
     public function processPay(Request $request) {
         
@@ -28,11 +29,11 @@ class HomeController extends Controller
 
         switch ($paymentType) {
             case 'first':
-                if($this->processPayment($request))
+                if($this->paymentService->processPayment($request))
                     return json_encode(['success' => true]);  
                 break;
             case 'second':
-                if($this->processPaymentAdditional($request))
+                if($this->paymentService->processPaymentAdditional($request))
                     return json_encode(['success' => true]);
                 break;
         }
@@ -40,93 +41,7 @@ class HomeController extends Controller
         return json_encode(['success' => false, 'msg' => 'Wrong params']);
     }  
 
-    private function isLimitToday() {
-        $countPaysToday = Pay::whereDate('created_at', Carbon::today())->where('payment_type', 'first')->count();
-        if($countPaysToday){
-            if($countPaysToday >= $this->limitPerDay)
-            {
-                echo json_encode(['success' => false, 'msg' => 'limit exceeded today']);
-                exit();              
-            }
-        }
-    }
-
-    private function isLimitTodayAdditional() {
-        $countPaysToday = Pay::whereDate('created_at', Carbon::today())->where('payment_type', 'second')->count();
-        if($countPaysToday){
-            if($countPaysToday >= $this->limitPerDayAdditional)
-            {
-                echo json_encode(['success' => false, 'msg' => 'limit exceeded today']);
-                exit();              
-            }
-        }
-    }
-
-    private function processPayment(Request $request) {
-
-        $dataParams = $request->all();
-        unset($dataParams['sign']);
-        ksort($dataParams);   
-        $dataStr = implode(':', $dataParams);
-        $dataStr .= $this->hashFirstPayMethod; 
-        $sha256 = hash('sha256', $dataStr);
-
-        if($sha256 !== $request->sign)
-            return false;
-
-        $this->isLimitToday();  
-
-        $pay = Pay::where('payment_type', 'first')->where('payment_id', $dataParams['payment_id'])->first();
-
-        if(!$pay)
-        {
-          $pay = new Pay;
-        }
-
-        $pay->payment_type = 'first';
-        $pay->merchant_id = $dataParams['merchant_id'];
-        $pay->payment_id = $dataParams['payment_id'];
-        $pay->status = $dataParams['status'];
-        $pay->amount = $dataParams['amount'];
-        $pay->amount_paid = $dataParams['amount_paid'];
-        $pay->save();
-
-        return true;
-    }
-
-
-    private function processPaymentAdditional(Request $request) {
-
-        $dataParams = $request->Body;
-        ksort($dataParams);   
-        $dataStr = implode('.', $dataParams);
-        $dataStr .= $this->hashSecondPayMethod; 
-        $md5_hash = md5($dataStr);
-
-        if($md5_hash !== $request->Headers['Authorization'])
-            return false;
-
-        $this->isLimitTodayAdditional();  
-
-        $pay = Pay::where('payment_type', 'second')->where('invoice', $dataParams['invoice'])->first();
-
-        if(!$pay)
-        {
-          $pay = new Pay;
-        }
-
-        $pay->payment_type = 'second';
-        $pay->project = $dataParams['project'];
-        $pay->invoice = $dataParams['invoice'];
-        $pay->status = $dataParams['status'];
-        $pay->amount = $dataParams['amount'];
-        $pay->amount_paid = $dataParams['amount_paid'];
-        $pay->save();
-
-        return true;
-    }
-
-    public function processPayRequest(Request $request) {
+    public function processPayRequest() {
         // тестовый запрос 
         $client = new Client();
 
@@ -147,9 +62,8 @@ class HomeController extends Controller
         }
     }  
     
-    public function processPayRequestAdditional(Request $request) {
+    public function processPayRequestAdditional() {
         // тестовый запрос
-
         $response = Http::post('http://app.loc/callback-url', [
             "Headers" => [
                 "Authorization" => 'd84eb9036bfc2fa7f46727f101c73c73',
